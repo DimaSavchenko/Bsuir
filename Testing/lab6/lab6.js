8f58c425c59d2c046e95805435b471434fb4d5dd
DimaSavchenko <dimasauchanka@gmail.com>
2016-11-14 13:47:53 +0300

    feat: update 6 and 8 labs

--- Testing/lab6/lab6.js
+++ Testing/lab6/lab6.js
@@ -1,64 +1,104 @@
 let seedString = '',
-  g = 160,
-  len = g/8,
-  sha1 = require('sha1');
-  crypto = require('crypto');
-  Buffer = require('buffer').Buffer;
-  bigInt = require('big-integer');
-
-while(len--) {
-  seedString += Math.random() < 0.5 ? '1' : '0';
-}
+    g = 160,
+    len = g / 8,
+    sha1 = require('sha1'),
+    crypto = require('crypto'),
+    Buffer = require('buffer').Buffer,
+    bigInt = require('big-integer'),
+    seed,
+    j = 3,
+    L = 512 + 64 * j,
+    n = bigInt(4),
+    b = bigInt(64),
+    pow2_g = bigInt(2).pow(g),
+    pow2_Lprev = bigInt(2).pow(L - 1);
 
 console.log(generateNum());
 
 function generateNum() {
-  let q = generateQ();
-  let p = generateP(q);
-  return p;
+
+    let q = generateQ();
+    let p = generateP(q);
+
+    return p;
 }
 
 function generateQ() {
-  let seed = bigInt(crypto.randomBytes(g/8).toString('hex'), 16);
-  let u = getHash(seed).xor(getHash(seed.next().mod(bigInt(2).pow(g))));11
-  let q = u.or(bigInt(2).pow(159)).or(bigInt.one);
 
-  return q;
+    let q;
+
+    do {
+        seed = bigInt(crypto.randomBytes(g / 8).toString('hex'), 16);
+        let u = getHash(seed).xor(getHash(seed.next().mod(bigInt(2).pow(g))));
+        q = u.or(bigInt(2).pow(159)).or(bigInt.one);
+
+    } while (!isProbablyPrime(q));
+
+    return q;
 }
 
 function generateP(q) {
-  let counter = 0;
-  let offset = 2;
-  while (counter < 4096) {
-    let W = bigInt.zero;
-    for (let i = 0; i < n; ++i) {
-      const vk = getHash(seed.add(offset + i).mod(pow2_g));
-      W = W.add(vk.multiply(bigInt(2).pow(i*160)));
+    let counter = 0;
+    let offset = 2;
+    while (counter < 4096) {
+        let W = bigInt.zero;
+        for (let i = 0; i < n; ++i) {
+            const vk = getHash(seed.add(offset + i).mod(pow2_g));
+            W = W.add(vk.multiply(bigInt(2).pow(i * 160)));
+        }
+        const lastVk = getHash(seed.add(offset + n).mod(pow2_g)).mod(bigInt(2).pow(b));
+        W = W.add(lastVk.multiply(bigInt(2).pow(n * 160)));
+        const X = W.add(pow2_Lprev);
+        const c = X.mod(q.multiply(2));
+        const p = X.minus(c.prev());
+        if (p.lt(pow2_Lprev) || !isProbablyPrime(p)) {
+            counter++;
+            offset += n + 1;
+            continue;
+        }
+        return p;
     }
-    const lastVk = getHash(seed.add(offset + n).mod(pow2_g)).mod(bigInt(2).pow(b));
-    W = W.add(lastVk.multiply(bigInt(2).pow(n*160)));
-    const X = W.add(pow2_Lprev);
-    const c = X.mod(q.multiply(2));
-    const p = X.minus(c.prev());
-    if (p.lt(pow2_Lprev) || !isProbablyPrime(p)) {
-      counter++;
-      offset += n + 1;
-      continue;
+    return null;
+}
+
+function isProbablyPrime(number) {
+    let j = 0;
+    let a = 0;
+    let m = bigInt(number).prev();
+    while (m.isEven()) {
+        m = m.divide(2);
+        a++;
     }
-    return p;
-  }
-  return null;
+
+    forCycle:
+        for (let i = 0; i < 70; ++i) {
+            const b = bigInt.randBetween(bigInt.one, number);
+            let z = b.modPow(m, number);
+            do {
+                if ((j === 0 && z.eq(1)) || z.eq(number.prev())) {
+                    continue forCycle;
+                } else if (j > 0 && z.eq(1)) {
+                    return false;
+                }
+                j++;
+                if (j < a) {
+                    z = z.square().mod(number);
+                }
+            } while (j < a);
+            return false;
+        }
+    return true;
 }
 
 function getHash(bigNumber) {
-  const sha1 = crypto.createHash('sha1');
-  let hexNumberString = bigNumber.toString(16);
-  if (hexNumberString.length % 2 === 1) {
-    hexNumberString = '0' + hexNumberString;
-  }
-  const buffer = Buffer.from(hexNumberString, 'hex');
-  sha1.update(buffer);
-  return bigInt(sha1.digest('hex'), 16);
+    const sha1 = crypto.createHash('sha1');
+    let hexNumberString = bigNumber.toString(16);
+    if (hexNumberString.length % 2 === 1) {
+        hexNumberString = '0' + hexNumberString;
+    }
+    const buffer = Buffer.from(hexNumberString, 'hex');
+    sha1.update(buffer);
+    return bigInt(sha1.digest('hex'), 16);
 }
 //let seed = seedString;
 //let u1 = sha1(seedString);
--- Testing/lab8/lab8.js
+++ Testing/lab8/lab8.js
@@ -2,6 +2,7 @@
 
 const {Builder, By, until} = require('selenium-webdriver');
 const test = require('selenium-webdriver/testing');
+const assert = require('selenium-webdriver/testing/assert');
 
 var webdriver = require('selenium-webdriver');
 var chrome = require('selenium-webdriver/chrome');
@@ -22,14 +23,41 @@ test.describe('GitHub', function() {
     test.it('login', function*() {
         yield driver.get('https://github.com/login');
         yield driver.findElement(By.id('login_field')).sendKeys('dimasauchanka@gmail.com');
-        yield driver.findElement(By.id('password')).sendKeys('');
+        yield driver.findElement(By.id('password')).sendKeys('dimentar_github_123');
         yield driver.findElement(By.name('commit')).click();
-        yield driver.wait(until.titleIs('GitHub'), 2000);
+        yield driver.wait(until.titleIs('GitHub'), 5000);
     });
 
-    test.it('git', function*() {
-        yield driver.get('https://github.com');
-        yield driver.findElement(By.name('q')).sendKeys('dimasauchanka@gmail.com');
+    test.it('search', function*() {
+        yield driver.findElement(By.name('q')).sendKeys('node');
+        yield driver.findElement(By.name('q')).sendKeys(webdriver.Key.RETURN);
+        yield driver.wait(until.titleContains('Search'), 5000);
+        let el = yield driver.findElement(By.name('q'));
+        let value = yield el.getAttribute('value');
+        assert(value).equalTo('node');
+    });
+
+    test.it('filter select', function*() {
+        yield driver.findElement(By.className('js-select-menu')).click();
+        yield driver.findElement(By.css('[href="https://github.com/search?o=desc&q=node&s=stars&type=Repositories&utf8=%E2%9C%93"]')).click();
+        //TODO: test most stars
+    });
+
+    test.it('click star', function*() {
+        yield driver.findElement(By.css('[href="https://github.com/nodejs/"]')).click();
+        yield driver.findElement(By.linkText('node')).click();
+        let el = yield driver.findElement(By.css('[href="/nodejs/node/stargazers"]')).click();
+        //console.log(el.innerHTML());
+        console.log(el.getText());
+    });
+    test.it('create Projects', function*() {
+        yield driver.get('https://github.com/DimaSavchenko/Bsuir');
+        yield driver.findElement(By.partialLinkText('Projects')).click();
+        yield driver.wait(until.titleContains('Projects'), 5000);
+        yield driver.findElement(By.linkText('Create a project')).click();
+        yield driver.findElement(By.id('project_name')).sendKeys('new project');
+        yield driver.findElement(By.css('.form-actions button')).click();
+        yield driver.wait(until.titleIs('new project'), 5000);
     });
 
     test.after(() => driver.quit());
